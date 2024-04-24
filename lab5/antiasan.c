792a22d3cc69329418813cf5f24239ba7311bc1a
fortanalaric <fortanalaric@gmail.com>
2024-04-24 20:21:29 +0800

    GO
    GO

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,5 +1,6 @@
 // TODO:
-void antiasan(unsigned long addr)
-{
-
+void antiasan(unsigned long addr) {
+    unsigned long shadow_addr = (addr >> 3) + 0x7ff8000000000000;
+    *((unsigned char*)shadow_addr) = 0x27;
 }
+
